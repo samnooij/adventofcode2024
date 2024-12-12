e3048e0dccf5c5901837f6b698b9e7408a12a35d
Sam Nooij <s.nooij@uu.nl>
2024-12-12 16:56:06 +0100

    Add second part

--- puzzle3.py
+++ puzzle3.py
@@ -11,8 +11,6 @@ overall_pattern = r"mul\([0-9]*,[0-9]*\)"
 
 numbers_pattern = r"mul\(([0-9]*),([0-9]*)\)"
 
-matches = []
-
 total_sum = 0
 
 with open(input_file, "r") as infile:
@@ -24,4 +22,42 @@ with open(input_file, "r") as infile:
 
             total_sum += mul(first_number, second_number)
 
+print("Total sum is (for the first part):")
 print(total_sum)
+
+
+## Part 2:
+
+do_dont_pattern = r"mul\([0-9]*,[0-9]*\)|don't\(\)|do\(\)"
+
+setting = "do"
+
+match_list = []
+
+with open(input_file, "r") as infile:
+    for line in infile:
+        hit_list = re.findall(do_dont_pattern, line)
+
+        for hit in hit_list:
+            match_list.append(hit)
+
+conditional_sum = 0
+
+for element in match_list:
+    if setting == "do":
+        if element == "do()":
+            pass
+        elif element == "don't()":
+            setting = "dont"
+        else:
+            first_number = int(re.search(numbers_pattern, element).group(1))
+            second_number = int(re.search(numbers_pattern, element).group(2))
+            conditional_sum += mul(first_number, second_number)
+    elif setting == "dont":
+        if element == "do()":
+            setting = "do"
+        else:
+            pass
+
+print("Sum taking into account dos and don'ts:")
+print(conditional_sum)
